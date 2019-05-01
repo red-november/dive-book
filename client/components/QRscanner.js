c8c5d11b03b4c3b8167f920026f05b993cc08a69
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-01 10:55:31 -0400

    set up qr component

---
+++ client/components/QRscanner.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class QRScanner extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+export default QRScanner
