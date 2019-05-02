cfbd4e55ff877e7f6557acf43f1c02741c6a4bd1
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-02 16:14:34 -0400

    AllShops component which displays all dive shops

---
+++ client/components/AllShops.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getShopsThunk} from '../store/index'
+
+class AllShops extends Comment {
+  componentDidMount() {
+    this.props.fetchShops()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>All Shops:</h1>
+        <ul>
+          {this.props.allShops.map(shop => (
+            <li key={shop.id}> {shop.name} </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  allShops: state.shops
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchShops: () => dispatch(getShopsThunk())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllShops)
--- client/store/index.js
+++ client/store/index.js
@@ -3,9 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
-import shopReducer from './diveShops'
+import shops from './diveShops'
 
-const reducer = combineReducers({user, shopReducer})
+const reducer = combineReducers({user, shops})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
