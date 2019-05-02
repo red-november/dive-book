67f4e807cfceebcfe2e1354d7b71fc8e0548fcf6
emily <emily@emilys-MacBook-Pro.local>
2019-05-02 16:34:20 -0400

    singleShop route

--- server/api/diveShops.js
+++ server/api/diveShops.js
@@ -1,11 +1,20 @@
 const router = require('express').Router()
-const {DiveShop} = require('../db/models')
+const { DiveShop } = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await DiveShop.findAll({})
-    res.json(users)
+    const shops = await DiveShop.findAll({})
+    res.json(shops)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:shopId', async (req, res, next) => {
+  try {
+    const singleShop = await DiveShop.findByPk(req.params.shopId)
+    res.json(singleShop)
   } catch (err) {
     next(err)
   }
