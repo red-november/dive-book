a6167f1b823d7e07b73c4af5ea829db3b5a8e31f
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-02 15:59:41 -0400

    diveshop reducer, thunk, actions

---
+++ client/store/diveShops.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+//ACTION TYPES
+
+const SET_SHOPS = 'SET_SHOPS'
+
+//ACTION CREATORS
+
+const setShops = shops => ({
+  type: SET_SHOPS,
+  shops
+})
+
+export const getShopsThunk = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/diveshops')
+    dispatch(setShops(data))
+  } catch (error) {
+    console.error('error in thunk')
+  }
+}
+
+const initialState = []
+
+const shopReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_SHOPS:
+      return action.shops
+    default:
+      return state
+  }
+}
+
+export default shopReducer
