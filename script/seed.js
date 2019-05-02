61a3dd129c0dddc4dd9554ec669a6e47de7e789d
emily <emily@emilys-MacBook-Pro.local>
2019-05-02 07:45:26 -0400

    seed data for diveshop/certification/offeredDive

--- script/seed.js
+++ script/seed.js
@@ -11,34 +11,52 @@ async function seed() {
     User.create({ email: 'cody@email.com', password: '123' }),
     User.create({ email: 'murphy@email.com', password: '123' })
   ])
-  // const diveShop = await Promise.all([
-  //   diveShop.create({
-  //     email: 'hawaii@email.com',
-  //     password: '123',
-  //     Name: 'Hawaii',
-  //     Location: 'Honolulu Street, Hawaii',
-  //     StoreFrontImgUrl: 'public/pictures/diveshop/hawaiiShop.jpg',
-  //     StampImgUrl: 'public/pictures/diveshop/hawaiiSymbol.png',
-  //   }),
-  //   diveShop.create({
-  //     email: 'maldives@email.com',
-  //     password: '123',
-  //     Name: 'Maldives',
-  //     Location: 'Maldives Street, Maldives',
-  //     StoreFrontImgUrl: 'public/pictures/diveshop/maldivesShop.jpeg',
-  //     StampImgUrl: 'public/pictures/diveshop/maldivesShopSymbol.jpg',
-  //   })
-  // ])
+  const diveShop = await Promise.all([
+    DiveShop.create({
+      email: 'hawaii@email.com',
+      password: '123',
+      name: 'Hawaii',
+      location: 'Honolulu Street, Hawaii',
+      storeFrontImgUrl: 'public/pictures/diveshop/hawaiiShop.jpg',
+      stampImgUrl: 'public/pictures/diveshop/hawaiiSymbol.png',
+    }),
+    DiveShop.create({
+      email: 'maldives@email.com',
+      password: '123',
+      name: 'Maldives',
+      location: 'Maldives Street, Maldives',
+      storeFrontImgUrl: 'public/pictures/diveshop/maldivesShop.jpeg',
+      stampImgUrl: 'public/pictures/diveshop/maldivesShopSymbol.jpg',
+    })
+  ])
 
-  // const certification = await Promise.all([
-  //   certification.create({
-  //     CertId: '001',
-  //     Provider: 'PADI',
-  //     Date: '2019-05-01',
-  //     Level: 'Advance',
-  //     InstructorId: '9999'
-  //   })
-  // ])
+  const certification = await Promise.all([
+    Certification.create({
+      certId: '001PADI',
+      provider: 'PADI',
+      date: '2019-05-01',
+      level: 'Advanced Skills Diver',
+      instructorId: 'James9999'
+    }),
+    Certification.create({
+      certId: '002SSI',
+      provider: 'SSI',
+      date: '2016-10-01',
+      level: 'Underwater Navigation',
+      instructorId: 'Dan8888'
+    }),
+  ])
+
+  const offeredDive = await Promise.all([
+    OfferedDive.create({
+      name: 'Barracuda Point',
+      description: 'Sipadan is a world-class destination, long attracting divers from around the world. Barracuda Point is one of the standout dive sites among many.'
+    }),
+    OfferedDive.create({
+      name: 'Blue Corner Wall',
+      description: 'Blue Corner Palau is one of the most action-packed scuba dive sites in the world and up to 13 different species of sharks circling just beyond the plummeting reef wall.'
+    }),
+  ])
 
 
 
