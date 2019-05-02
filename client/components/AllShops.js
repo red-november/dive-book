599deabe0df9b33eb7528cbd949e6c2ed8718f3a
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-02 16:18:20 -0400

    bug fix for allshops component

--- client/components/AllShops.js
+++ client/components/AllShops.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getShopsThunk} from '../store/index'
 
-class AllShops extends Comment {
+class AllShops extends Component {
   componentDidMount() {
     this.props.fetchShops()
   }
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Scanner} from './components'
+import {Login, Signup, UserHome, Scanner, AllShops} from './components'
 import {me} from './store'
 
 /**
@@ -20,6 +20,7 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route path="/qr" component={Scanner} />
+        <Route path="/allshops" component={AllShops} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
