691c74149a9d2c5179dbe26161aea3de77437f0a
emily <emilylu369@gmail.com>
2019-05-04 17:53:32 -0400

    get all offerredDives routes done

--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ router.use('/diveshops', require('./diveShops'))
 router.use('/logs', require('./logs'))
 router.use('/certs', require('./certifications'))
 router.use('/badges', require('./badges'))
+router.use('/offeredDives', require('./offeredDives'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
---
+++ server/api/offeredDives.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const {OfferedDive, Diver} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  const allOfferedDives = await OfferedDive.findAll()
+  res.status(200).send(allOfferedDives)
+})
+
+router.get('/diveshops/:diveshopId', async (req, res, next) => {
+  const diveShopId = Number(req.params.diveshopId)
+
+  const OfferedDive = await OfferedDive.findAll({
+    where: {diveshopId: diveShopId}
+  })
+  res.status(200).send(OfferedDive)
+})
