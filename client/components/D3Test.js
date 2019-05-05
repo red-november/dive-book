748ff464078e936ef2d6d83fceced3ac744659d8
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-04 22:55:56 -0400

    donut chart test for logs

--- client/components/D3Test.js
+++ client/components/D3Test.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import {Donut} from 'britecharts-react'
+import {Donut, Legend} from 'britecharts-react'
 
 export const CircleChart = ({data}) => {
   return (
-    <Donut data={data} margin={{top: 30, bottom: 30, left: 30, right: 30}} />
+    <div>
+      <Donut
+        data={data}
+        margin={{top: 30, bottom: 30, left: 30, right: 30}}
+        colorSchema={[
+          '#ccf7f6',
+          '#70e4e0',
+          '#00d8d2',
+          '#00acaf',
+          '#007f8c',
+          '#005e66',
+          '#003c3f',
+          '#002d2f',
+          '#0d2223'
+        ]}
+      />
+      <Legend data={data} />
+    </div>
   )
 }
