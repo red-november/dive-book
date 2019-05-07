2c9fed8a26af0b22554126df9bac9e8f6a46d62e
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-07 11:51:42 -0400

    search functionality for observations

--- client/components/ObservationSearch.js
+++ client/components/ObservationSearch.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getObservationsThunk} from '../store'
 
-const ObservationSearch = props => {
-  return <h1>Add Observation:</h1>
+class ObservationSearch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentList: []
+    }
+    this.keyup = this.keyup.bind(this)
+  }
+  componentDidMount() {
+    this.props.fetchObservations()
+  }
+
+  keyup(evt) {
+    let word = evt.target.value
+    let newList = this.props.observations.filter(obs => {
+      if (obs.name.toLowerCase().includes(word.toLowerCase())) {
+        return obs.name
+      }
+    })
+    this.setState({currentList: newList})
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="search">Add an observation</label>
+        <input type="text" name="search" onKeyUp={this.keyup} />
+        <ul>
+          {this.state.currentList.map(obs => <li key={obs.id}>{obs.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
