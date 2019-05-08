3ea0c9b99f0fd3b6199d0cd1c9e82253e9ac2428
Jan Czarina Javier <janczarinajavier@Jans-MacBook-Pro.local>
2019-05-08 11:17:32 -0400

    Merged Master

--- client/components/TestMap.js
+++ client/components/TestMap.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = ''
+// mapboxgl.accessToken = '' <--- Provide Access Token to line 22
 
 class TestMap extends Component {
   state = {
@@ -19,7 +19,7 @@ class TestMap extends Component {
     return (
       <ReactMapGL
         {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})} mapboxApiAccessToken='pk.eyJ1IjoiYXp1cmVzb2x1dGUiLCJhIjoiY2p0NGluc3Y5MWFuZTQ0bWp4cWpuczJibSJ9.6gFyZERSJnukMTuti2vJSQ'/>
+        onViewportChange={(viewport) => this.setState({viewport})} mapboxApiAccessToken='PUT TOKEN HERE'/>
     );
   }
 }
