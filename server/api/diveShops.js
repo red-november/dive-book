250fe9e76cbc97c2cffc4de5a65c0630c7ca3316
Roumesh Persand <rpersand25@gmail.com>
2019-05-10 11:32:08 -0400

    added nearest diveshop api route

--- server/api/diveShops.js
+++ server/api/diveShops.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const {DiveShop, OfferedDive} = require('../db/models')
+const db = require('../db')
+const Sequelize = require('sequelize')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -19,5 +21,21 @@ router.get('/:shopId', async (req, res, next) => {
     res.json(singleShop)
   } catch (err) {
     next(err)
+  }
+})
+
+router.get('/nearest/:coords', async (req, res, next) => {
+  try {
+    const [long, lat] = req.params.coords.split(',')
+    const nearest = await db.query(
+      `SELECT (ST_DISTANCE(ST_GeogFromText('SRID=4326;POINT(${Number(
+        long
+      )} ${Number(lat)})'), geog)) AS dist, geog FROM diveshops
+      ORDER BY dist LIMIT 1;`,
+      {type: Sequelize.QueryTypes.SELECT}
+    )
+    res.json(nearest)
+  } catch (error) {
+    next(error)
   }
 })
