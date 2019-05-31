b76ae10c701629568518cb09ffd47500c587e701
Harrison Cole <harrison.d.cole@gmail.com>
2019-05-31 16:25:32 -0400

    test for load data class method

--- server/api/divers.spec.js
+++ server/api/divers.spec.js
@@ -6,7 +6,29 @@ const db = require('../db')
 const app = require('../index')
 const Diver = db.model('diver')
 
-describe('User routes', () => {
+async function before() {
+  await db.sync({force: true})
+  await Diver.LoadData([
+    {
+      firstName: 'Fred',
+      lastName: 'Astaire',
+      email: 'fred@fred.com',
+      password: 'dance',
+      height: 60,
+      weight: 150
+    },
+    {
+      firstName: 'Rocky',
+      lastName: 'Raccoon',
+      email: 'rocky@fred.com',
+      password: 'Gideon',
+      height: 40,
+      weight: 20
+    }
+  ])
+}
+
+describe('Diver routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
@@ -30,3 +52,27 @@ describe('User routes', () => {
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
+
+describe('Class methods', () => {
+  beforeEach(async () => {
+    await before()
+  })
+
+  describe('Load data', () => {
+    it('creates divers for each element in the array', () => {
+      return Diver.findOne({
+        where: {
+          firstName: 'Rocky'
+        }
+      }).then(diver => {
+        expect(diver.firstName).to.equal('Rocky')
+      })
+    })
+
+    it('creates divers for each element in the array', () => {
+      return Diver.findByPk(2).then(diver =>
+        expect(diver.firstName).to.equal('Fred')
+      )
+    })
+  }) //end describe load data
+}) //end describe class methods
