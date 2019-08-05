d098167269e9e22a77f002efd553f2cbc699485a
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-08-05 13:48:01 -0400

    Reconfiguring Sequelize to be Mac-compatible

--- server/db/db.js
+++ server/db/db.js
@@ -4,23 +4,23 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 // Use this for local windows. Change the username and pw to your postgres admin:
-const db = new Sequelize(
-  process.env.DATABASE_URL || `${databaseName}`,
-  'postgres',
-  'qwertyuiop',
-  {
-    logging: false,
-    dialect: 'postgres'
-  }
-)
-
-// Use this for local mac or Heroku:
 // const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   process.env.DATABASE_URL || `${databaseName}`,
+//   'postgres',
+//   'qwertyuiop',
 //   {
-//     logging: false
+//     logging: false,
+//     dialect: 'postgres'
 //   }
 // )
+
+// Use this for local mac or Heroku:
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
 
 module.exports = db
 
